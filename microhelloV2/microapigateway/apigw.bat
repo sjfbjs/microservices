de29e1de97259a8e14154d860b6210ea05885ae2
shisuan <shisuan@myweimai.com>
2021-04-18 11:00:13 +0800

    修改解析micro api namespace 的规则
    服务注册后的名字
    sjfbjs.com.api拆解为
    --namespace sjfbjs
    --type  api

--- microhelloV2/microapigateway/apigw.bat
+++ microhelloV2/microapigateway/apigw.bat
@@ -2,6 +2,6 @@
 # gin beego 等
 set MICRO_REGISTRY=etcd
 set MICRO_REGISTRY_ADDRESS=127.0.0.1:2379
-set MICRO_API_NAMESPACE=sjfbjs.com.api
+set MICRO_API_NAMESPACE=sjfbjs.com
 set MiCRO_API_HANDLER=rpc
-micro api --namespace sjfbjs.com.api
+micro api --namespace sjfbjs.com --type api
